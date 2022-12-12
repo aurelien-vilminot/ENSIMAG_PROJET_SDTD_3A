db30d75216373545227b58d175e7b548453d6a50
brunoal <alexis.bruno@grenoble-inp.org>
2022-12-12 13:20:47 +0000

    Remove prometheus machine + Install docker on every machine of the cluster

--- deploiement/terraform/scripts/cluster_launcher.sh
+++ deploiement/terraform/scripts/cluster_launcher.sh
@@ -14,3 +14,12 @@ wget https://raw.githubusercontent.com/kubernetes-sigs/kubespray/master/requirem
 wget "https://raw.githubusercontent.com/kubernetes-sigs/kubespray/master/$(cat requirements.txt)" -O install_requirements.txt
 
 pip3 install -r install_requirements.txt;
+
+# Install Docker Engine
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
--- deploiement/terraform/scripts/prometheus_launcher.sh
+++
@@ -1,8 +0,0 @@
-# Change to restart automically deamons services
-sudo sed -i "/.*nrconf{restart}.*/c\$nrconf{restart} = 'a';" /etc/needrestart/needrestart.conf;
-
-# Update environment
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y;
-
-# Initialize Prometheus
-sudo apt install -yq prometheus;

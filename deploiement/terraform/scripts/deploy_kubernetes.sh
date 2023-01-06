702661ac2ba11ecb3c892e230b707f4866fce669
brunoal <alexis.bruno@grenoble-inp.org>
2023-01-06 19:00:12 +0000

    Expose Prometheus + fix firewall

--- deploiement/terraform/scripts/deploy_kubernetes.sh
+++ deploiement/terraform/scripts/deploy_kubernetes.sh
@@ -4,6 +4,14 @@ readonly username="kubespray"
 # Prometheus pods
 echo "Deploy Prometheus..."
 # TODO Arthur + Laure (helm déjà installé sur la workstation)
+kubectl create ns monitoring
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm -n monitoring install kube-prometheus-stack prometheus-community/kube-prometheus-stack
+# TODO wait kube-prometheus-stack-prometheus pods running
+sleep 60
+# TODO fix port 32000
+kubectl patch svc kube-prometheus-stack-prometheus -n monitoring -p '{"spec": {"type": "NodePort"}}}'
 echo "Done.\n"
 
 # App pods

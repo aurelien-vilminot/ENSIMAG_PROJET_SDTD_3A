44cf46d60f588c9c440d4d2153de86405c0e13ca
Arthur Sarry <arthur.sarry@gmail.com>
2023-01-16 10:56:23 +0100

    fix: Wait on the proper pod and remove unnecessary closing curly brace.

--- deploiement/terraform/scripts/deploy_kubernetes.sh
+++ deploiement/terraform/scripts/deploy_kubernetes.sh
@@ -7,8 +7,8 @@ kubectl create ns monitoring
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm -n monitoring install kube-prometheus-stack prometheus-community/kube-prometheus-stack
-kubectl -n monitoring wait --for=condition=Running pod/kubep-prometheus-stack-prometheus-0 --timeout=300s
-kubectl patch svc kube-prometheus-stack-prometheus -n monitoring -p '{"spec": {"type": "NodePort", "ports": [{"nodePort": 32000, "port": 9090, "protocol": "TCP", "targetPort": 9090}]}}}'
+kubectl -n monitoring wait --for=condition=Running pod/prometheus-kube-prometheus-stack-prometheus-0 --timeout=300s
+kubectl patch svc kube-prometheus-stack-prometheus -n monitoring -p '{"spec": {"type": "NodePort", "ports": [{"nodePort": 32000, "port": 9090, "protocol": "TCP", "targetPort": 9090}]}}'
 echo "Done.\n"
 
 # App pods
